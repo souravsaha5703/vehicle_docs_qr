34c7f151fa26d8e7675fdef8c2465338de34d842
Sourav Saha <saha83787@gmail.com>
2024-04-07 18:33:16 +0530

    authentication added

--- userPanel/app.js
+++ userPanel/app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const path=require("path");
 const session=require("express-session");
+const cookieParser=require("cookie-parser");
 const router=require("./routes/index");
 const userRouter=require("./routes/users");
 
@@ -10,6 +11,7 @@ const PORT=7000;
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(session({
     secret:'rto user info',
