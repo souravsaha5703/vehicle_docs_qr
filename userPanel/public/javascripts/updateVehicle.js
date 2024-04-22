febb40c73092c4c122725c3b6df3a53d91cd33e0
Sourav Saha <saha83787@gmail.com>
2024-04-22 20:31:50 +0530

    static files updated

---
+++ userPanel/public/javascripts/updateVehicle.js
@@ -0,0 +1,74 @@
+const updatebtns=document.querySelectorAll(".updateBtn");
+const updateModel=document.getElementById("updateModal");
+const closeModal=document.getElementById("closeModal");
+
+updatebtns.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        updateModel.classList.remove("hidden");
+        updateModel.classList.add("block");
+        getData(e.target.id);
+    });
+});
+
+async function getData(id){
+    try{
+        const response=await fetch(`/vehicleData/${id}`);
+
+        if(!response.ok){
+            throw new Error('Failed to fetch data');
+        }
+        const data=await response.json();
+        setValue(data);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+function setValue(data){
+    let regDate=new Date(data.reg_upto);
+    let taxDate=new Date(data.taxPaidUpto);
+    let insuranceDate=new Date(data.insurancePaidUpto);
+    let puccDate=new Date(data.pucValidUpto);
+    let formattedRegDate=regDate.toISOString().split('T')[0];
+    let formattedTaxPaidDate=taxDate.toISOString().split('T')[0];
+    let formattedInsurancePaidDate=insuranceDate.toISOString().split('T')[0];
+    let formattedPuccDate=puccDate.toISOString().split('T')[0];
+    document.getElementById("pollutionUpto").value=formattedPuccDate;
+    document.getElementById("registrationUpto").value=formattedRegDate;
+    document.getElementById("taxpaidUpto").value=formattedTaxPaidDate;
+    document.getElementById("insurancepaidUpto").value=formattedInsurancePaidDate;
+    document.getElementById("vehicleId").value=data._id;
+}
+
+closeModal.addEventListener("click",(e)=>{
+    e.preventDefault();
+    updateModel.classList.remove("block");
+    updateModel.classList.add("hidden");
+});
+
+
+document.getElementById("updateForm").addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let id=document.getElementById("vehicleId").value;
+    let pollutionUpto=document.getElementById("pollutionUpto").value;
+    let registrationUpto=document.getElementById("registrationUpto").value;
+    let taxpaidUpto=document.getElementById("taxpaidUpto").value;
+    let insurancePaidUpto=document.getElementById("insurancepaidUpto").value;
+
+    const updatedVehicleData={pollutionUpto,registrationUpto,taxpaidUpto,insurancePaidUpto};
+
+    fetch(`/updateVehicle/${id}`,{
+        method:'POST',
+        headers:{'Content-Type': 'application/json'},
+        body:JSON.stringify(updatedVehicleData)
+    })
+    .then(res=>{
+        if(res.redirected){
+            window.location.href=res.url;
+        }
+    })
+    .catch(error=>{
+        console.error(error);
+    });
+});
