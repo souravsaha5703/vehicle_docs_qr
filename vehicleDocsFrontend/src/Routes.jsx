a7e092559ea6f6bd57b24fb2efa984b5093048e7
Sourav Saha <saha83787@gmail.com>
2024-10-31 18:50:28 +0530

    routes updated

--- vehicleDocsFrontend/src/Routes.jsx
+++ vehicleDocsFrontend/src/Routes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Login from './Pages/Authentication/Login';
 import VerifyOTP from './Pages/Authentication/VerifyOTP';
+import ProtectedRoutes from './ProtectedRoutes';
+import Dashboard from './Pages/Admin/Dashboard';
 
 const AppRouter = () => {
     return (
@@ -10,6 +12,9 @@ const AppRouter = () => {
                 <Route path='/' element={<App />}>
                     <Route path='' element={<Login />} />
                     <Route path='verifyotp' element={<VerifyOTP />} />
+                    <Route path='/admin' element={<ProtectedRoutes />}>
+                        <Route path='dashboard' element={<Dashboard />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
