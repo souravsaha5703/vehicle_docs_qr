cbaee078c1faf658fbb3dcf088ca7073f34b7423
Sourav Saha <saha83787@gmail.com>
2024-10-31 18:50:40 +0530

    session management done

--- vehicleDocsFrontend/src/App.jsx
+++ vehicleDocsFrontend/src/App.jsx
@@ -1,11 +1,50 @@
 import './App.css';
 import { Outlet } from "react-router-dom";
+import { AppContext } from './Context/AppContextProvider';
+import Loader from './components/Loader/Loader';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const { setLoggedIn } = useContext(AppContext);
 
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    try {
+      axios.get("http://localhost:7000/getAdmin", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+        .then(res => {
+          setLoggedIn(true);
+          console.log(res.data.message);
+          setTimeout(() => {
+            setLoading(false)
+          }, 3000);
+        })
+        .catch(error => {
+          setLoggedIn(false);
+          setTimeout(() => {
+            setLoading(false);
+          }, 3000);
+          console.log(error);
+        })
+    } catch (error) {
+      console.error(error);
+    }
+  }, [])
   return (
     <>
-      <Outlet/>
+      {loading ? (
+        <div className="w-full h-screen flex items-center justify-center">
+          <Loader color="text-blue-600" />
+        </div>
+      ) : (
+        <Outlet />
+      )}
     </>
   )
 }
